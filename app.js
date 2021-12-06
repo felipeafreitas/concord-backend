3d6706ba59902c8a4b7d451aefd2b8b765900aa9
Felipe Freitas <afelipe.freitas@gmail.com>
2021-12-06 19:45:38 -0300

    adding cors package

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ require("dotenv").config();
 const express = require('express')
 const bodyParser = require('body-parser')
 require("./config/db.config")();
+const cors = require("cors")
 
 const app = express()
 
 app.use(bodyParser.json())
+
+app.use(cors())
 
 const userRouter = require("./routes/user.routes");
 app.use("/", userRouter);
